3feca705722ba006d2fa31ea77328f5aff8b6f19
hnk1203 <hnk1203@naver.com>
2023-07-19 19:55:42 +0900

    useEffect 배우기, 컴포넌트 연결 복습하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { useState, useEffect } from "react";
+import styles from "./App.module.css"
 import Button from "./Button";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => {
+    console.log(" irun once");
+  }
+  useEffect(()=> {
+    console.log("call THE API");
+  }, []);
   return (
-   <div>
-    <h1>
-      Welcome back!!!
-    </h1>
-    <Button text={"Continue"} />
-   </div>
+    <div>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+      <Button text={"빡빡이 아저씨"}/>
+    </div>
   );
 }
 
--- src/Button.js
+++ src/Button.js
@@ -1,14 +1,12 @@
 import  PropTypes  from "prop-types";
+import  styles  from "./Button.module.css";
 
 function Button({text}) {
-    return <button style={{
-        backgroundColor:"toamto",
-        color:"white", 
-    }}>{text}</button>;
+    return <button className={styles.btn}>{text}</button>;
 }
 
 Button.propTypes = {
     text : PropTypes.string.isRequired,
 };
 
-export default Button;
+export default Button;  
