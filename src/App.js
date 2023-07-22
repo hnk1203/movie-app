446ddd578fe03d94156709153d9b59640a24b0e9
hnk1203 <hnk1203@naver.com>
2023-07-22 21:57:36 +0900

    useEffect Deps 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import { useState, useEffect } from "react";
-import styles from "./App.module.css"
+import styles from "./App.module.css";
 import Button from "./Button";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
   const iRunOnlyOnce = () => {
     console.log(" irun once");
-  }
-  useEffect(()=> {
+  };
+  useEffect(() => {
     console.log("call THE API");
   }, []);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5) {
+    console.log("search for", keyword);
+    }
+  }, [keyword]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="search here!"
+      />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me</button>
-      <Button text={"빡빡이 아저씨"}/>
+      <Button text={"빡빡이 아저씨"} />
     </div>
   );
 }
