a9cc46fe7c6bc717a933d6cd73437ee103142923
hnk1203 <hnk1203@naver.com>
2023-07-27 19:50:57 +0900

    코인계산기

--- src/App.js
+++ src/App.js
@@ -1,30 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState();
+  const [selectedCoin, setSelectedCoin] = useState(null); // 선택된 코인 정보를 저장하는 상태
+
+  const onChange = (event) => setMoney(event.target.value);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const onSelectChange = (event) => {
+    const selectedCoinId = event.target.value; // 선택된 코인의 ID
+    const selectedCoinInfo = coins.find((coin) => coin.id === selectedCoinId); // 선택된 코인 정보
+    setSelectedCoin(selectedCoinInfo); // 선택된 코인 정보를 상태로 저장
   };
-  console.log(toDos);
+
+  const calcAmount = () => {
+    if (selectedCoin && money) {
+      const priceUSD = selectedCoin.quotes.USD.price;
+      const canAmount = parseFloat(money) / priceUSD;
+      return canAmount.toFixed(2); // 살 수 있는 양을 소수점 두 자리까지 표시
+    }
+    return "0.00";
+  };
+
   return (
     <div>
-      <h1>my To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="write your to do"
-        />
-        <button>Add To Do</button>
-      </form>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <input onChange={onChange} type="number" placeholder="소지한 달러" value={money} />
+          <strong> $</strong>
+          <select onChange={onSelectChange}>
+            <option value="">코인 선택</option>
+            {coins.map((coin) => (
+              <option key={coin.id} value={coin.id}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <h3>살 수 있는 {selectedCoin.symbol} 의 양: {calcAmount()} {selectedCoin ? selectedCoin.symbol : ""}</h3>
+        </div>
+      )}
     </div>
   );
 }
