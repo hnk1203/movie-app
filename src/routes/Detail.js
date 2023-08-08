ba70d09be0a483354fe8a9010af3c31245db1fcc
hnk1203 <hnk1203@naver.com>
2023-08-08 21:46:09 +0900

    Detail 추가하기

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
 
 function Detail() {
+  const [Loading, setLoading] = useState(true);
+  const [infos, setInfos] = useState();
   const { id } = useParams();
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
+    setInfos(json.data.movie);
+    setLoading(false);
   };
+  console.log(infos);
   useEffect(() => {
     getMovie();
   }, []);
 
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      {Loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+            <Movie
+              key={infos.id}
+              id={infos.id}
+              coverImg={infos.medium_cover_image}
+              title={infos.title}
+              genres={infos.genres}
+            />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
